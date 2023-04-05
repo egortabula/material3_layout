33548d15b77aac6f1c04b8edc998dd3ec37d5213
Egor Tabula <64892468+egortabula@users.noreply.github.com>
2023-04-05 14:11:18 +0700

    add padding to pane container widget

--- lib/src/widgets/pane_container_widget.dart
+++ lib/src/widgets/pane_container_widget.dart
@@ -5,23 +5,25 @@ import 'package:material3_layout/src/theme/surface_color_enum.dart';
 class PaneContainerWidget extends StatelessWidget {
   final Widget child;
   final SurfaceColorEnum surfaceColor;
-
+  final EdgeInsetsGeometry padding;
   const PaneContainerWidget({
     Key? key,
     required this.child,
     this.surfaceColor = SurfaceColorEnum.surface,
+    this.padding = EdgeInsets.zero,
   }) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-        width: double.infinity,
-        height: double.infinity,
-        decoration: BoxDecoration(
-          color: NewSurfaceTheme.getSurfaceColor(surfaceColor, context),
-          borderRadius: const BorderRadius.all(Radius.circular(12)),
-        ),
-        child: child,
-      );
+      padding: padding,
+      width: double.infinity,
+      height: double.infinity,
+      decoration: BoxDecoration(
+        color: NewSurfaceTheme.getSurfaceColor(surfaceColor, context),
+        borderRadius: const BorderRadius.all(Radius.circular(12)),
+      ),
+      child: child,
+    );
   }
 }
