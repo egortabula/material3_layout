0e013b4bddeea501a761e1a9c2ff06e248ac2ada
Egor Tabula <64892468+egortabula@users.noreply.github.com>
2023-04-05 20:25:38 +0700

    update docs

--- lib/src/layouts/page_layout.dart
+++ lib/src/layouts/page_layout.dart
@@ -1,18 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:material3_layout/src/breakpoints.dart';
+import 'package:material3_layout/src/layouts/layout.dart';
 
-import 'layout.dart';
 
 /// A widget that displays a different layout based on the screen size.
 class PageLayout extends StatelessWidget {
   /// The layout to use when the screen size is compact.
+  /// Typically `SinglePaneLayout`
   final Layout compactLayout;
 
   /// The layout to use when the screen size is medium.
+  /// 
+  /// Tipically `SinglePaneLayout`, but in some case `SplitPaneLayout` or `TwoPaneLayout`
   final Layout? mediumLayout;
 
   /// The layout to use when the screen size is extended.
+  /// 
+  /// Typically `TwoPaneLayout`. but it can be `SinglePaneLayout` or `SplitPaneLayout``
   final Layout? extendedLayout;
 
   const PageLayout({
